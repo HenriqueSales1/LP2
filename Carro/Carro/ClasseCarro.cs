015182ca5357fd16e049527b1f53982c5f068a39
Henrique <103958824+HenriqueSales1@users.noreply.github.com>
2024-07-25 17:33:29 -0300

    Add files via upload

---
+++ Carro/Carro/ClasseCarro.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Carro
+    {
+        private int velocidade = 0;
+        private bool ligado = false;
+        private int marcha = 1;
+        private readonly int aceleracao = 3;
+
+        public Carro(int velocidade, bool ligado, int marcha)
+        {
+            this.velocidade = velocidade;
+            this.ligado = ligado;
+            this.marcha = marcha;
+        }
+
+        public int Velocidade
+        {
+            get { return velocidade; }
+            set { velocidade = value; 
+            }
+        }
+
+        public bool Ligado {
+            get { return ligado; } 
+            set { ligado = value; } 
+        }
+        public int Marcha {
+            get {  return marcha; } 
+            set {  marcha = value; } 
+        }
+
+        public void ligar()
+        {
+            ligado = true;
+        }
+
+        public void acelerar()
+        {
+            if(ligado) velocidade += aceleracao;
+        }
+
+        public void freiar()
+        {
+            if(velocidade != 0) velocidade -= aceleracao;
+        }
+
+        public void mudaMarcha()
+        {
+            if (marcha < 6) marcha++;
+            else marcha = 0;
+        }
+    }
+}
---
+++ Carro/Carro/TelaCarro.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class TelaCarro : Form
+    {
+        private Carro carro;
+        public TelaCarro()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            carro = new Carro(0, false, 0);
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+
+            if (carro.Ligado) label2.Text = "Ligado";
+            else label2.Text = "Desligado";
+
+            label5.Text = Convert.ToString(carro.Velocidade);
+            marcha();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            carro.ligar();
+
+            if (carro.Ligado) label2.Text = "Ligado";
+            else label2.Text = "Desligado";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            carro.acelerar();
+
+            label5.Text = Convert.ToString(carro.Velocidade);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            carro.freiar();
+
+            label5.Text = Convert.ToString(carro.Velocidade);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            carro.mudaMarcha();
+            marcha();
+        }
+
+        private void marcha()
+        {
+            
+            if (carro.Marcha == 0) label6.Text = "Ré";
+            else label6.Text = Convert.ToString(carro.Marcha);
+        }
+    }
+}
